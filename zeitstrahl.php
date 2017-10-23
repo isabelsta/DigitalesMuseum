5d2c817631610ec3ab4463595c2a51a32162b6ad
caroroe <it16154@lehre.dhbw-stuttgart.de>
2017-10-23 15:30:59 +0200

    Timeline (mit Inhalt)

--- zeitstrahl.php
+++ zeitstrahl.php
@@ -29,81 +29,41 @@
     <div class="timeline">
         <div class="column">
             <div class="title">
-                <h1> 1867 </h1>
-                <h2> ALFRED ELY BEACH: <br> BEACH PNEUMATIC TRANSIT </h2>
+                <h1> 1859 </h1>
+                <h2> BERTHA <br> PAPPENHEIM </h2>
             </div>
             <div class="description">
-                <p> Americký vynálezca v tomto roku prezentoval tunelový systém, v ktorom sa pomocou stlačeného vzduchu pohybovala transportná bunka valcovitého tvaru. Na dráhe premávala kyvadlovým spôsobom jedna transportná kapsula. Pohyb zabezpečoval pneumatický kompresor, ktorý v jednom smere kapsulu “vytláčal” a v druhom smere ”nasával”. Bola vybudovaná aj skúšobná dráha, ktorá istý čas fungovala ale projekt bol zamietnutý a v dôsledku krachu americkej burzy sa ho nepodarilo oživiť. </p>
+                <p> Als Gründerin des jüdischen Frauenbundes (1916) gab Bertha Pappenheim den entscheidenden Anstoß zur Gründung der "Zentralwohlfahrtsstelle der deutschen Juden" (ZWST) ein Jahr später, um die vielen Wohlfahrtsvereine organisatorisch zusammenzubringen. </p>
             </div>
         </div>
 
         <div class="column">
             <div class="title">
-                <h1> 1904 </h1>
-                <h2> ROBERT H. GODDARD: <br> VACUUM TUBE TRANSPORTATION SYSTEM </h2>
+                <h1> 1870 </h1>
+                <h2> MARIA <br> MONTESSORI </h2>
             </div>
             <div class="description">
-                <p> Prvé zmienky o jeho koncepte sa objavili v roku 1909 ale kompletná dokumnetácia pochádza z roku 1904, ktorá sa objavila až 30 rokov po jeho smrti v opustenom sklade. V tomto koncepte Goddard popisuje hlavné črty konceptu Hyperloop.  Spomína použitie lineárneho elektromotora. Opisuje zariadenie na minimalizáciu nepriaznivého prúdenia vzduchu v okolí transportnej kapsuly. Venuje sa aj použitiu vzduchových ložísk. Hovorí o cestovaní rýchlosťou 1900 km/h. </p>
+                <p> Maria Montessori prägte als Frau, Kinderärztin und Pädagogik weltweit pädagogische Institutionen der Heil-, Schul- und Sozialpädagogik. 1900 gründete sie ein pädagogisch-medizinisches Institut und Modellschule. Hier wurden auch Lehrer ausgebildet, die mit behinderten Kindern arbeiteten. Je länger Montessori mit behinderten Kindern arbeitete, desto größer wurde ihr Interesse an allgemeiner Pädagogik. 1907 eröffnete sie in Rom das Kinderhaus "casa bambini". Kinder, ob behindert oder nichtbehindert, sollen Montessori zufolge nach dem Prinzip der Selbstständigkeit in altersgemischten Gruppen lernen. Dazu gestalten die Fachkräfte die entsprechende Lernumgebung. Montessori schuf den Begriff der "vorbereiteten Umgebng", der auch die Haltung der Fachkräfte thematisiert. Sie entwickelte umfangreiches Spielmaterial, das Kinder anregen und zu experimentellen Versucen auffordern sollte. </p>
             </div>
         </div>
 
         <div class="column">
             <div class="title">
-                <h1> 1909 <!-- – 1914 --> </h1>
-                <h2> BORIS WEINBERG: <br> POHYB BEZ TRENIA </h2>
+                <h1> 1912 </h1>
+                <h2> RUTH CHARLOTTE <br> COHN </h2>
             </div>
             <div class="description">
-                <p> Boris Weinberg vo svojej práci  vydanej v roku 1914, písal o levitujúcich automobiloch na elektromagnetický pohon, pohybujúcich sa v medenom potrubí, z ktorého bol odsatý vzduch. Experimentoval s 10 kg vozňom, ktorý sa pohyboval v medenom potrubí . Tento systém bol kruhový a vožeň dosahoval maximálne 6 km/h. Tvrdil, že na 3km trati vozeň dosiahne až 800-1000 km/h. Projekt nemohli zrealizovať kvôli vysokým finančným nákladom. </p>
+                <p> Die Jüdin Ruth Cohn emigrierte 1933 aus Deutschland zunächst nach Zürich und dann in die USA. Dort begann Cohn Aspekte der Gruppen-, Gestalt-, und Familientherapie mit psychoanalytischen Kenntnissen zu verbinden und für pädagogische Aufgaben fruchtbar zu machen. 1975 beschrieb sie ihren beruflichen Weg in dem Buch "Von der Psychoanalyse zur themenzentrierten Interaktion", als deren Begründerin sie gilt. TZI ist ein Handlungskonzept, um lebendiges Lernen in und durch Gruppen zu unterstützen. Cohn ging davon aus, dass ein konstruktives Miteinander von Menschen nur entstehen kann, wenn das Individuum (das Ich), die Gruppe (das Wir) und die Sache (das Es bzw. Thema) gleichrangige Beachtung erfahren. Gerahmt wird dieses Dreieck vom Globe, der die gesellschaftlichen Bedinungen symbolisiert. </p>
             </div>
         </div>
 
         <div class="column">
             <div class="title">
-                <h1> 1969 </h1>
-                <h2> JOHN A. VOLPE: <br> STREAMLINERS WITHOUT WHEELS </h2>
+                <h1> 1953 </h1>
+                <h2> MICHAEL <br> WINKLER </h2>
             </div>
             <div class="description">
-                <p> Volpe detailne popisuje koncepty, ale aj prototypy dopravných prostriedkov, ktoré sa mali pohybovať na vzduchových „vankúšoch“ či „ložiskách“. A to nad otvorenou traťou alebo v polouzavretom a uzavretom potrubí. Spomína lineárne indukčné motory, pneumatický pohon, transportné potrubné systémy s minimálnou atmosférou či dokonca vákuom. Uvádza, že rýchlosti týchto vozidiel sa môžu pohybovať od 300 do 800 km/h. Ukončenie konceptov majú na svedomí financie alebo technika. </p>
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="title">
-                <h1> 1972 <!-- – 1978 --> </h1>
-                <h2> ROBERT M. SALTER: <br> THE VERY HIGH SPEED TRANSIT SYSTEM </h2>
-            </div>
-            <div class="description">
-                <p> Jednalo sa o víziu podzemnej tunelovej prepravy obrovskou rýchlosťou,  až 16000 km/h. Technológiu nazval "plávanie po magnetickej dráhe" a prepravný modul "gondolou".Za kľúčové považoval minimalizovanie energie nutnej na prevádzku. Preto navrhol vytvoriť v tuneli atmosféru, aká panuje v maximálnej nadmorskej výške, v ktorej lietajú dopravné lietadlá. Tým by sa minimalizoval odpor vzduchu. Aby ešte ušetril ďalšiu energiu, potreboval eliminovať trenie. Riešením boli „elektromagnetické lôžka. </p>
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="title">
-                <h1> 1974 <!-- - 2009 --> </h1>
-                <h2> SWISSMETRO </h2>
-            </div>
-            <div class="description">
-                <p> Swissmetro bol/je švajčiarsky vactrain projekt cez 100 km dlhý nízkotlakový tunel. Pomocou lineárnych elektromotorov mala rýchlosť súpravy presahovať 520 km/h. Na rozdiel od iných svetových vactrain projektov, plánovali Švajčiari vybudovať tunel pre štandardne veľké vlakové súpravy. Tunel teda musel mať priemer 5 až 6 m. Iniciátorom projektu bol Rodolphe Nieth, ktorý pracoval ako inžinier pre Švajčiarske spolkové dráhy. Projekt bol oficiálne ukončený v roku 2009. </p>
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="title">
-                <h1> 1997 </h1>
-                <h2> DARYL OSTER: <br> EVACUATED TUBE TRANSPORT TECHNOLOGIES (ET3) </h2>
-            </div>
-            <div class="description">
-                <p> Opäť sa jedná o nízkotlakový potrubný systém, v ktorom sa pohybuje transportná kapsula, za pomoci lineárnych elektromotorov a magnetickej levitácie (maglev). Tzn., že kapsula nemusí mať žiadne pohyblivé alebo elektricky aktivované komponenty – pasívne supravodiče umožňujú kapsule vznášať sa v trubici, pričom vírivé prúdy indukované vo vodivých materiáloch umožňujú riadenie kapsuly. Na jej rozbeh by sa z veľkej časti využívala energia, ktorá vzniká pri brzdení (rekuperácia). </p>
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="title">
-                <h1> 2003 </h1>
-                <h2> MICHAEL THALHAMMER: <br> TUBEWAY </h2>
-            </div>
-            <div class="description">
-                <p> Tubeway podobne ako Hyperloop využíva systém transportného potrubia. Vo vnútri transportného potrubia sa pohybuje kapsula pomocou pneumatického pohonu. Kompresor zabudovaný v kapsule nasáva vzduch z prednej časti kapsuly, ktorý stlačí a uvoľní zo zadnej časti kapsuly. Kapsula v potrubí nelevituje, pohybuje sa pomocou kolies. Jej rýchlosť sa má pohybovať od 40 do 350 km/h. Tubeway je však stále len v rovine teoretickej a experimentálnej práce. </p>
+                <p> Michael Winkler zeigte 1988 in seiner Theorie der Sozialpädagogik die Schwierigkeiten auf, den Begriff, den Gegenstand, die Vorraussetzungen und die Wirkung sozialpädagogischen Handelns wissenschaftstheoretisch zu bestimmen. Winklers Verständnis von Sozialpädagigik bündelt sich im Begriff des Diskurses: Sozialpädagogik ist für ihn, was sich als Sozialpädagogik bestimmt. Winkler begreift Sozialpädagogik als Prinzip des Denkens, als Organisation von Gedanken. Mit dem Begriffspaar "Subjekt" und "Ort" zeigt er die Problem- und Handlusngsstruktur der Sozialpädagogik auf. Entsprechend benennen "Aneignung" und "Vermittlung" den Modus sozialpädagogischen Handelns. Winkler verweist auf die Notwendigkeit zur Gestaltung des Sozialen. Sozialpädagogisches Handeln muss seiner Auffassung nach vor dem Hintergrung gesellschaftlicher Verhältnisse gestaltet werden. </p>
             </div>
         </div>
     </div>
